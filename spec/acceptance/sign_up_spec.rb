994e60cf2d4b546f98d2a62393bea5f3a7e91b90
DenisRebenok <denis.rebenok.pro@gmail.com>
2016-03-25 18:39:10 +0200

    feature 'User sign_up'

---
+++ spec/acceptance/sign_up_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User sign_up', %q{
+  In order to be able to sign up
+  As an new user
+  I want to be able to register
+} do
+
+  given(:user) { build(:user) }
+  given(:registered_user) { create(:user) }
+
+  scenario 'New user try to sign up with valid data' do
+    sign_up(user)
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Registered user try to sign up again' do
+    sign_up(registered_user)
+
+    expect(page).to have_content 'Email has already been taken'
+  end
+
+  scenario 'invalid data' do
+    sign_up(User.new(email: '', password: '', password_confirmation: ''))
+
+    expect(page).to have_content 'errors prohibited this user from being saved'
+  end
+end
