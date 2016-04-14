3f38c58814eb5070a619e222e4592e148033eaac
DenisRebenok <denis.rebenok.pro@gmail.com>
2016-04-14 16:33:45 +0300

    fix tests & add destroing attachments files via ajax

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -16,12 +16,16 @@ feature 'Add files to answer', %q{
 
   scenario 'User adds file when create answer', js: :true do
     fill_in 'Your answer', with: 'answer text'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+
+    all("input[type='file']").first.set("#{Rails.root}/spec/spec_helper.rb")
+    all("input[type='file']").last.set("#{Rails.root}/spec/rails_helper.rb")
 
     click_on 'Post'
 
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
 end
--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -13,13 +13,17 @@ feature 'Add files to question', %q{
     visit(new_question_path)
   end
 
-  scenario 'User adds file when asks question' do
+  scenario 'User adds files when asks question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+
+    all("input[type='file']").first.set("#{Rails.root}/spec/spec_helper.rb")
+    all("input[type='file']").last.set("#{Rails.root}/spec/rails_helper.rb")
 
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
 end
